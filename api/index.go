c227bcb3d2efa7b9e7e9ba1c5a9cc3a9af6d47b9
Victor Jose <vicjocaso@gmail.com>
2023-10-15 14:18:28 -0400

    Added HTMX

--- api/index.go
+++ api/index.go
@@ -2,14 +2,57 @@ package handler
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"text/template"
+	"time"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go RunTime Serverless!</h1>")
+type Film struct {
+	Title    string
+	Director string
 }
 
+// func Handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "<h1>Hello from Go RunTime Serverless!</h1>")
+// }
+
+// func main() {
+// 	http.HandleFunc("/", Handler)
+// 	http.ListenAndServe(":3000", nil)
+// }
+
 func main() {
-	http.HandleFunc("/", Handler)
-	http.ListenAndServe(":3000", nil)
+	fmt.Println("Go app...")
+
+	// handler function #1 - returns the index.html template, with film data
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		films := map[string][]Film{
+			"Films": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Blade Runner", Director: "Ridley Scott"},
+				{Title: "The Thing", Director: "John Carpenter"},
+			},
+		}
+		tmpl.Execute(w, films)
+	}
+
+	// handler function #2 - returns the template block with the newly added film, as an HTMX response
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+		// htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white'>%s - %s</li>", title, director)
+		// tmpl, _ := template.New("t").Parse(htmlStr)
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+	}
+
+	// define handlers
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film/", h2)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+
 }
