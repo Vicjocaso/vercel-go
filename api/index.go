f16b1c832a3546c4545ed603965b14e45da8b172
Victor Jose <vicjocaso@gmail.com>
2023-10-15 14:23:37 -0400

    rewrite func

--- api/index.go
+++ api/index.go
@@ -22,36 +22,36 @@ type Film struct {
 // 	http.ListenAndServe(":3000", nil)
 // }
 
+// handler function #1 - returns the index.html template, with film data
+func Handler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("../list.html"))
+	films := map[string][]Film{
+		"Films": {
+			{Title: "The Godfather", Director: "Francis Ford Coppola"},
+			{Title: "Blade Runner", Director: "Ridley Scott"},
+			{Title: "The Thing", Director: "John Carpenter"},
+		},
+	}
+	tmpl.Execute(w, films)
+}
+
+// handler function #2 - returns the template block with the newly added film, as an HTMX response
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(1 * time.Second)
+	title := r.PostFormValue("title")
+	director := r.PostFormValue("director")
+	// htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white'>%s - %s</li>", title, director)
+	// tmpl, _ := template.New("t").Parse(htmlStr)
+	tmpl := template.Must(template.ParseFiles("../list.html"))
+	tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+}
+
 func main() {
 	fmt.Println("Go app...")
 
-	// handler function #1 - returns the index.html template, with film data
-	h1 := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("../list.html"))
-		films := map[string][]Film{
-			"Films": {
-				{Title: "The Godfather", Director: "Francis Ford Coppola"},
-				{Title: "Blade Runner", Director: "Ridley Scott"},
-				{Title: "The Thing", Director: "John Carpenter"},
-			},
-		}
-		tmpl.Execute(w, films)
-	}
-
-	// handler function #2 - returns the template block with the newly added film, as an HTMX response
-	h2 := func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(1 * time.Second)
-		title := r.PostFormValue("title")
-		director := r.PostFormValue("director")
-		// htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white'>%s - %s</li>", title, director)
-		// tmpl, _ := template.New("t").Parse(htmlStr)
-		tmpl := template.Must(template.ParseFiles("../list.html"))
-		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
-	}
-
 	// define handlers
-	http.HandleFunc("/", h1)
-	http.HandleFunc("/add-film/", h2)
+	http.HandleFunc("/", Handler)
+	http.HandleFunc("/add-film/", handlerPost)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 
