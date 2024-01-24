f93e88e7378a9d29a628e7c117a21b27f7af871e
Victor J. Calderon <vicjocaso@gmail.com>
2024-01-23 20:43:01 -0500

    vercer.json

--- api/main.go
+++ api/main.go
@@ -6,19 +6,19 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func Handler(c echo.Context) error {
+func handleEndpoint1(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello from endpoint 1!")
 }
 
-func HandleEndpoint2(c echo.Context) error {
+func handleEndpoint2(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello from endpoint 2!")
 }
 
 func main() {
 	e := echo.New()
 
-	e.GET("/1", Handler)
-	e.GET("/2", HandleEndpoint2)
+	e.GET("/handleEndpoint1", handleEndpoint1)
+	e.GET("/handleEndpoint2", handleEndpoint2)
 
 	e.Logger.Fatal(e.Start(":3000"))
 }
