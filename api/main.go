1cefcfc6c991a17d3c0768f2146adf3800d72964
Victor J. Calderon <vicjocaso@gmail.com>
2024-01-23 21:11:03 -0500

    roll back

--- api/main.go
+++ api/main.go
@@ -9,15 +9,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
 
-// func HandleEndpoint2(c echo.Context) error {
-// 	return c.String(http.StatusOK, "Hello from endpoint 2!")
-// }
-
-// func main() {
-// 	e := echo.New()
-
-// 	e.GET("/endpoint1", Handler)
-// 	e.GET("/endpoint2", HandleEndpoint2)
-
-// 	e.Logger.Fatal(e.Start(":3000"))
-// }
+func main() {
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":3000", nil)
+}
