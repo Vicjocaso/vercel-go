09711bf785c5216d5e00312d501335c6f882b947
Victor J. Calderon <vicjocaso@gmail.com>
2024-01-23 21:03:59 -0500

    s

--- api/main.go
+++ api/main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func HandleEndpoint1(c echo.Context) error {
+func Handler(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello from endpoint 1!")
 }
 
@@ -17,7 +17,7 @@ func HandleEndpoint2(c echo.Context) error {
 func main() {
 	e := echo.New()
 
-	e.GET("/endpoint1", HandleEndpoint1)
+	e.GET("/endpoint1", Handler)
 	e.GET("/endpoint2", HandleEndpoint2)
 
 	e.Logger.Fatal(e.Start(":3000"))
