47575d5dba97f676728b47a90016223ef11795d4
Victor J. Calderon <vicjocaso@gmail.com>
2024-01-23 21:16:14 -0500

    some

--- api/index.go
+++ api/index.go
@@ -1,15 +1,39 @@
+// package handler
+
+// import (
+// 	"fmt"
+// 	"net/http"
+// )
+
+// func Handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+// }
+
+//	func main() {
+//		http.HandleFunc("/", Handler)
+//		http.ListenAndServe(":3000", nil)
+//	}
 package handler
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+func handleEndpoint1(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello from endpoint 1!")
+}
+
+func handleEndpoint2(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello from endpoint 2!")
 }
 
 func main() {
-	http.HandleFunc("/", Handler)
-	http.ListenAndServe(":3000", nil)
+	e := echo.New()
+
+	e.GET("/endpoint1", handleEndpoint1)
+	e.GET("/endpoint2", handleEndpoint2)
+
+	e.Logger.Fatal(e.Start(":3000"))
 }
